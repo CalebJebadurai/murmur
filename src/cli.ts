82e4148a4a2a277efb39f67af62d27b7a85dfb1b
Caleb Jebadurai Nickson <calebjebadurai@gmail.com>
2026-09-05 08:08:02 +0530

    chore(release): publish murmr 0.2.1

--- src/cli.ts
+++ src/cli.ts
@@ -13,7 +13,7 @@ import { hookCommand, type HookAction } from "./commands/hook.ts";
 import { toolCommand } from "./commands/tool.ts";
 import { docsCommand } from "./commands/docs.ts";
 
-const VERSION = "0.2.0";
+const VERSION = "0.2.1";
 
 type Flags = { positionals: string[]; flags: Record<string, string | boolean> };
 
